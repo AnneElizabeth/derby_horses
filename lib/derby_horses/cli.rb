515d418042edb43ae9b3e92a9cbc8aafad461412
Anne Edmondson <edmondson.anne@gmail.com>
2019-05-02 01:40:15 +0000

    Add list of horses.

--- lib/derby_horses/cli.rb
+++ lib/derby_horses/cli.rb
@@ -1,7 +1,37 @@
 class DerbyHorses::CLI
 
   def start
-    puts "Hello World"
+    puts "Hello Kentucky Derby fan!"
+    puts "The race horses competing in the 2019 Kentucky Derby are:"
+    puts ""
+    puts "War of Will"
+    puts "Tax"
+    puts "By My Standards"
+    puts "Gray Magician"
+    puts "Improbable"
+    puts "Vekoma"
+    puts "Maximum Security"
+    puts "Tacitus"
+    puts "Plus Que Parfait"
+    puts "Cutting Humor"
+    puts "Haikal"
+    puts "Omaha Beach"
+    puts "Code of Honor"
+    puts "Win Win Win"
+    puts "Master Fencer JPN"
+    puts "Game Winner"
+    puts "Roadster"
+    puts "Long Range Toddy"
+    puts "Spinoff"
+    puts "Country House"
+    puts "Bodexpress"
+    puts "Signalman"
+    puts "Anothertwistafate"
+    puts "Sueno"
+    puts "Bourbon War"
+    puts "git@github.com:AnneElizabeth/derby_horses.git"
+    puts "Type in the name of a horse you would like to know more about."
+    input = gets.strip.downcase
   end
 
 end
