4c05528aeabc5f659f9fd158a67e9849f117681a
Anne Edmondson <edmondson.anne@gmail.com>
2019-05-02 04:22:59 +0000

    Add web_page.css selectors.

--- lib/derby_horses/scraper.rb
+++ lib/derby_horses/scraper.rb
@@ -3,7 +3,15 @@
 class DerbyHorses::Scraper
 
   def self.scrape_horse_info(horse_url)
-    puts "In the Scraper class."
+    web_page = Nokogiri::HTML(open(horse_url))
+    web_page.css("div.owner-cell a").text
+    web_page.css("div.trainer-cell a").text
+    web_page.css("div.breeder-cell").text
+    #breeder.css("div.text-style-insight-label").text
+    web_page.css("div.inset-cell p")[0].text
+    web_page.css("div.inset-cell p")[1].text
+    web_page.css("div.inset-cell p")[2].text
+    binding.pry
 
   end
 
