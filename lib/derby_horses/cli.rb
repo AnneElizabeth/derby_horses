eaec840a90a5911a052fd59f23d85ba1add6fec3
Anne Edmondson <edmondson.anne@gmail.com>
2019-05-03 05:30:23 +0000

    Creating instances of Horses.

--- lib/derby_horses/cli.rb
+++ lib/derby_horses/cli.rb
@@ -11,59 +11,32 @@ class DerbyHorses::CLI
   end
 
   def list_horses
-    puts <<~ DOC
-    puts "1. War of Will"
-    puts "2. Tax"
-    puts "3. By My Standards"
-    puts "4. Gray Magician"
-    puts "5. Improbable"
-    puts "6. Vekoma"
-    puts "7. Maximum Security"
-    puts "8. Tacitus"
-    puts "9. Plus Que Parfait"
-    puts "10. Cutting Humor"
-    puts "11. Haikal"
-    puts "12. Omaha Beach"
-    puts "13. Code of Honor"
-    puts "14. Win Win Win"
-    puts "15. Master Fencer JPN"
-    puts "16. Game Winner"
-    puts "17. Roadster"
-    puts "18. Long Range Toddy"
-    puts "19. Spinoff"
-    puts "20. Country House"
-    puts "21. Bodexpress"
-    puts "22. Signalman"
-    puts "23. Anothertwistafate"
-    puts "24. Sueno"
-    puts "25. Bourbon War"
-    puts ""
-    DOC
+
+    @horses = DerbyHorses::Horses.current
   end
 
   def menu
-    puts "Enter the number of the horse you would like to know more about./nIf you'd like to see the list of horses again, enter LIST./nexEnter EXIT to exit the program."
     input = nil
     while input != "exit"
+      puts "Enter the number of the horse you would like to know more about./nIf you'd like to see the list of horses again, enter LIST./nexEnter EXIT to exit the program."
       input = gets.strip.downcase
       case input
-        when "1"
-          puts "horse owner, trainer, breeder, current odds, profile for Will at War"
-          horse_url = "https://www.kentuckyderby.com/horses/war-of-will"
-          horse_info = DerbyHorses::Scraper.scrape_horse_info(horse_url)
-            #puts @owner
+      when "1"
+        puts "horse owner, trainer, breeder, current odds, profile for Will at War"
+        horse_url = "https://www.kentuckyderby.com/horses/war-of-will"
+        horse_info = DerbyHorses::Scraper.scrape_horse_info(horse_url)
+      when "2"
+        puts "Tax is a horse."
+      when "3"
+        puts "By My Standards is a horse."
+      when "list" || "List" || "LIST"
+        list_horses
+      else
+        puts "Whoops. That's an invalid entry. Please try again."
+    end
+  end
 
-        when "2"
-          puts "Tax is a horse."
-        when "3"
-          puts "By My Standards is a horse."
-        else
-          puts "Oops! Invalid input. Please try again."
-        end
-      end
-    end
-    
-    def goodbye
-      puts "Enjoy the 145th running of the Kentucky Derby!"
-    end
+  def goodbye
+    puts "Enjoy the 145th running of the Kentucky Derby!"
+  end
 end
--- lib/derby_horses/horses.rb
+++ lib/derby_horses/horses.rb
@@ -1,3 +1,34 @@
 class DerbyHorses::Horses
+  def self.current
+    #returns all instances of Horses
 
+    puts <<~DOC
+    1. War of Will
+    2. Tax
+    DOC
+
+    horse_1 = self.new
+    horse_1.name = "War of Will"
+    horse_1.owner = "Gary Barber"
+    horse_1.trainer = "Mark E. Casse"
+    horse_1.breeder = "Flaxman Holdings Limited"
+    horse_1.sex = "Colt"
+    horse_1.color = "Bay"
+    horse_1.birthplace = "Kentucky"
+    horse_1.foaldate = "April 17, 2016"
+    horse_1.profile = "horse profile text"
+    horse_1.url = "https://www.kentuckyderby.com/horses/war-of-will"
+
+    horse_2 = self.new
+    horse_2.name = "War of Will"
+    horse_2.owner = "Gary Barber"
+    horse_2.trainer = "Mark E. Casse"
+    horse_2.breeder = "Flaxman Holdings Limited"
+    horse_2.sex = "Colt"
+    horse_2.color = "Bay"
+    horse_2.birthplace = "Kentucky"
+    horse_2.foaldate = "April 17, 2016"
+    horse_2.profile = "horse profile text"
+    horse_2.url = "https://www.kentuckyderby.com/horses/war-of-will"
+  end
 end
