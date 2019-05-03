b11d59237228a2e24135fbb8674cc78db81d979d
Anne Edmondson <edmondson.anne@gmail.com>
2019-05-03 04:48:55 +0000

    Re-designing structure.

--- lib/derby_horses/cli.rb
+++ lib/derby_horses/cli.rb
@@ -1,9 +1,17 @@
+#CLI Controller
+
 class DerbyHorses::CLI
 
   def start
     puts "Hello Kentucky Derby fan!"
     puts "The race horses competing in the 2019 Kentucky Derby are:"
-    puts ""
+    list_horses
+    menu
+    goodbye
+  end
+
+  def list_horses
+    puts <<~ DOC
     puts "1. War of Will"
     puts "2. Tax"
     puts "3. By My Standards"
@@ -30,21 +38,32 @@ class DerbyHorses::CLI
     puts "24. Sueno"
     puts "25. Bourbon War"
     puts ""
-    puts "Type in the number of the horse you would like to know more about."
-    input = gets.strip
-    case input
-      when "1"
-        puts "Will at War is a horse."
-        horse_url = "https://www.kentuckyderby.com/horses/war-of-will"
-        horse_info = DerbyHorses::Scraper.scrape_horse_info(horse_url)
-        puts horse_info
-      when "2"
-        puts "Tax is a horse."
-      when "3"
-        puts "By My Standards is a horse."
-      else
-        puts "Oops! Invalid input. Please try again."
-      end
+    DOC
   end
 
+  def menu
+    puts "Enter the number of the horse you would like to know more about./nIf you'd like to see the list of horses again, enter LIST./nexEnter EXIT to exit the program."
+    input = nil
+    while input != "exit"
+      input = gets.strip.downcase
+      case input
+        when "1"
+          puts "horse owner, trainer, breeder, current odds, profile for Will at War"
+          horse_url = "https://www.kentuckyderby.com/horses/war-of-will"
+          horse_info = DerbyHorses::Scraper.scrape_horse_info(horse_url)
+            #puts @owner
+
+        when "2"
+          puts "Tax is a horse."
+        when "3"
+          puts "By My Standards is a horse."
+        else
+          puts "Oops! Invalid input. Please try again."
+        end
+      end
+    end
+    
+    def goodbye
+      puts "Enjoy the 145th running of the Kentucky Derby!"
+    end
 end
--- lib/derby_horses/scraper.rb
+++ lib/derby_horses/scraper.rb
@@ -7,7 +7,7 @@ class DerbyHorses::Scraper
     web_page.css("div.breeder-cell").text
     #breeder.css("div.text-style-insight-label").text
     array_of_p = web_page.css("div.inset-cell p")[0..2].text
-    array_of_p.map do |horse_profile|
+    array_of_p.tap do |horse_profile|
       DerbyHorses::Profile.new(horse_profile)
     end
 
